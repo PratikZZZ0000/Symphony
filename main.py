72b373038daa377efe3b5c4a6bad23542b423e20
pratikpro2004 <>
2021-11-05 13:52:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import discord 
+import os
+import random
+from webserver import keep_alive
+
+
+Token = os.environ['Token']
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print("We've logged in as {0.user}".format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:     # The bot user
+    return
+  
+  introductions = ["hello","hi","hey","hey!","hi!","hello!"]
+
+  responses = ["Hello there!","Hey, what's up?","How're you doing?","How are you?","Hi, what's the plan for today?"]
+
+  if message.content in introductions:
+    response = random.choice(responses)
+    await message.channel.send(response)
+
+keep_alive()
+client.run(Token)
+
+
